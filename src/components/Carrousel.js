77d602c9cc94b53279e1a162444f949dd09092f4
BlueAzrell <azarielazul@gmail.com>
2022-09-03 15:32:15 -0300

    Commit de tailwindCSS y el daisyUI

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,34 +1,26 @@
 const Carrousel = () => {
   return (
-    <div class="carousel w-full">
-  <div id="slide1" class="carousel-item relative w-full">
-    <img src="https://live.staticflickr.com/65535/48047838153_e3eb647208_k.jpg" class="w-full" alt="El logo de la marca PelidaMax"/>
-    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" class="btn btn-circle">❮</a> 
-      <a href="#slide2" class="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide2" class="carousel-item relative w-full">
-    <img src="https://clongeek.com/wp-content/uploads/2020/01/Vengeance-6100-1.jpg" alt="cyberbros" border="0" class="w-full"/>
-    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" class="btn btn-circle">❮</a> 
-      <a href="#slide3" class="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" class="carousel-item relative w-full">
+    <div>
+<div class="carousel w-full">
+  <div id="item1" class="carousel-item w-full">
     <img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/ryzen_4000.jpg" class="w-full" alt="El logo de la marca PelidaMax"/>
-    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" class="btn btn-circle">❮</a> 
-      <a href="#slide4" class="btn btn-circle">❯</a>
-    </div>
   </div> 
-  <div id="slide4" class="carousel-item relative w-full">
-    <img src="https://cdn.wccftech.com/wp-content/uploads/2018/06/Cyberpunk-2077_2018_06-12-18_006-2060x1159.jpg" alt="Que arrecho" border="0" class="w-full"/>
-    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" class="btn btn-circle">❮</a> 
-      <a href="#slide1" class="btn btn-circle">❯</a>
-    </div>
+  <div id="item2" class="carousel-item w-full">
+    <img src="https://www.rabinsxp.com/medias/AMD-Ryzen-7000-Processors-Price-Launch-Date.jpg" class="w-full" alt="El logo de la marca PelidaMax"/>
+  </div> 
+  <div id="item3" class="carousel-item w-full">
+    <img src="https://cdn.eteknix.com/wp-content/uploads/2020/11/ryzen-cpu-1.jpg" class="w-full" alt="El logo de la marca PelidaMax"/>
+  </div> 
+  <div id="item4" class="carousel-item w-full">
+    <img src="https://pcpla.net/wp-content/uploads/2021/03/69904/intel-raptor-lake-cpus-spotted-likely-to-be-alder-lakes-successor-1536x864.jpg" class="w-full" alt="El logo de la marca PelidaMax"/>
   </div>
+</div> 
+<div class="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" class="btn btn-xs">1</a> 
+  <a href="#item2" class="btn btn-xs">2</a> 
+  <a href="#item3" class="btn btn-xs">3</a> 
+  <a href="#item4" class="btn btn-xs">4</a>
+</div>
 </div>
   )
 }
